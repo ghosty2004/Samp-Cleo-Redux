f6dfcb90f17fa84fa8f0a88f88e627c139b2ab48
Ghosty2004 <fejerszilveszter@gmail.com>
2022-10-22 12:18:50 +0300

    Added some more natives

--- includes/sampNatives.js
+++ includes/sampNatives.js
@@ -20,9 +20,44 @@ export const sampAddChatMessage = (text, color) => native("sampAddChatMessage", 
 export const sampSendChat = (text) => native("sampSendChat", text);
 
 /**
- * 
+ * Register a client command
  * @param {string} cmd 
  * @param {void} func 
  * @returns {boolean}
  */
 export const sampRegisterChatCommand = (cmd, func) => native("sampRegisterChatCommand", cmd, func);
+
+/**
+ * Sends a request to the server to change the skin.
+ * @param {number} id
+ */
+export const sampRequestClass = (id) => native("sampRequestClass", id);
+
+/**
+ * Sends information about the modification of t / s to the server.
+ * @param {number} event 
+ * @param {number} id 
+ * @param {number} param1 
+ * @param {number} param2 
+ */
+export const sampSendScmEvent = (event, id, param1, param2) => native("sampSendScmEvent", event, id, param1, param2);
+
+/**
+ * Sets a special action to a local player
+ * @param {number} action 
+ */
+export const sampSetSpecialAction = (action) => native("sampSetSpecialAction", action);
+
+/**
+ * Sends death information from a player with the specified cause to the server
+ * @param {number} playerId 
+ * @param {number} reason 
+ */
+export const sampSendDeathByPlayer = (playerId, reason) => native("sampSendDeathByPlayer", playerId, reason);
+
+/**
+ * Gets a t / s hand on it. If the car is not in the stream zone, then returns 0.
+ * @param {number} id 
+ * @returns {number}
+ */
+export const sampGetCarHandleBySampVehicleId = (id) => native("sampGetCarHandleBySampVehicleId", id);
--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 /// <reference path=".config/sa.d.ts" />
 
-import { isSampAvailable, sampAddChatMessage, sampRegisterChatCommand } from "./includes/sampNatives";
- 
-function test() {
-    sampAddChatMessage("work", 0x00CC00);
-}
+import { isSampAvailable, sampAddChatMessage } from "./includes/sampNatives";
+
 
 while(true) {
     wait(0);
     if(!isSampAvailable()) continue;
-
     sampAddChatMessage("Ok", 0xFF0000);
-
-    sampRegisterChatCommand("test", test);
-
     break;
 }
