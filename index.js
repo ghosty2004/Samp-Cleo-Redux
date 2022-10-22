4a60fb851f67002e1942fa2f6e05ae4d6ecf2d76
Ghosty2004 <fejerszilveszter@gmail.com>
2022-10-22 22:01:45 +0300

    Edited

--- index.js
+++ index.js
@@ -14,24 +14,7 @@ while(true) {
         wait(0);
 
         if(Pad.IsKeyDown(KeyCode.End)) {
-            const char = new Player().getChar();
-            //Samp.addChatMessage(`${JSON.stringify(Samp.getAnimationNameAndFile(1))}`, 0xFF0000);
-            //Samp.addChatMessage(`${Object.entries(GameState).find(f => f[1] == Samp.getGameState())[0]}`, 0xFF0000);
-            //Samp.addChatMessage(`${Samp.getCurrentDialogId()}`, 0xFF0000);
-            //Samp.addChatMessage(`${Object.entries(DialogStyle).find(f => f[1] === Samp.getCurrentDialogType())[0]}`, 0xFF0000);
-            //Samp.addChatMessage(`${JSON.stringify(Samp.isDialogActive())}`, 0xFF0000);
-            //Samp.setCurrentDialogEditboxText("test");
-            //Samp.addChatMessage(`${Samp.getCurrentDialogEditboxText()}`, 0xFF0000);
-            //Samp.addChatMessage(`${Samp.getCurrentDialogListItem()}`, 0xFF0000);
-            //Samp.closeCurrentDialogWithButton(1);
-            //Samp.addChatMessage(`${Samp.is3dTextDefined(1)}`, 0xFF0000);
-            //Samp.addChatMessage(`${Samp.create3dText("Hi", -1, char.getCoordinates().x, char.getCoordinates().y, char.getCoordinates().z, 50)}`);
-            //Samp.addChatMessage(`${JSON.stringify(Samp.hasDialogRespond(1))}`, 0xFF0000);
-            //Samp.addChatMessage(`${Samp.getCurrentServerAddress()}`, 0xFF0000);
-            //Samp.showDialog(1, "Salut frate", "Salut frate", "Ok", "Close", DialogStyle.MSGBOX);
-            //Samp.addChatMessage(`${Samp.getCharHandleBySampPlayerId(3)}`, 0xFF0000);
-            //Samp.addChatMessage(Samp.getCharHandleBySampPlayerId(3).getHealth().toString(), 0xFF0000);
-            //Samp.addChatMessage("You have successfully pressed button \"END\"", 0x00CC00);
+            Samp.addChatMessage("Pressed button \"END\"!", 0x00CC00);
         }
     }
 }
