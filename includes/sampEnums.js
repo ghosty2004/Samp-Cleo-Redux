ccc702860ef55c72b2a25aaa3ba1d6bdc7edc026
Ghosty2004 <fejerszilveszter@gmail.com>
2022-10-22 15:44:26 +0300

    Added more natives

---
+++ includes/sampEnums.js
@@ -0,0 +1,8 @@
+export const GameState = {
+    "NONE": 0,
+    "WAIT_CONNECT": 1,
+    "AWAIT_JOIN": 2,
+    "CONNECTED": 3,
+    "RESTARTING": 4,
+    "DISCONNECTED": 5
+}
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 /// <reference path=".config/sa.d.ts" />
 
-import { isSampAvailable, sampAddChatMessage } from "./includes/sampNatives";
+import { KeyCode } from "./.config/enums";
+
+import { isSampAvailable, sampAddChatMessage, sampGetPlayerArmour, sampGetPlayerHealth, sampGetPlayerNickname, sampSendSpawn } from "./includes/sampNatives";
 
 
 while(true) {
     wait(0);
     if(!isSampAvailable()) continue;
     sampAddChatMessage("Ok", 0xFF0000);
+
+    while(true) {
+        wait(0);
+        sampAddChatMessage(`${JSON.stringify(sampGetPlayerNickname(100))}`, 0x00CC00);
+        //sampAddChatMessage(`${Zone.GetName(0, 0, 0)}`, 0xFF0000);
+        
+        if(Pad.IsKeyDown(KeyCode.End)) {
+            sampAddChatMessage("Key end was pressed", 0x00CC00);
+            sampSendSpawn();
+        }
+    
+    }
     break;
 }
